c538b690744789415dce50a202206fe7d8513335
Pedro Pires (onelikeandidie) <onelikeandidie@proton.me>
2024-12-03 17:00:17 +0000

    Added: Advent of Code day 1 part 1

---
+++ day-1/src/main.rs
@@ -0,0 +1,86 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Path of the file to get
+    #[arg(short, long)]
+    path: std::path::PathBuf,
+    /// Solver
+    #[arg(short, long, value_enum)]
+    solver: Solver,
+}
+
+#[derive(Debug, Clone, clap::ValueEnum)]
+enum Solver {
+    Part1,
+    Part2,
+}
+
+impl Solver {
+    pub fn solve(&self, input: String) -> String {
+        match self {
+            Solver::Part1 => {
+                let input: Vec<(i64, i64)> = input
+                    .split("\n")
+                    .filter(|line| !line.is_empty())
+                    .map(|line| line.split_once(" ").unwrap())
+                    .map(|(left, right)| (left.trim(), right.trim()))
+                    .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
+                    .collect();
+                let mut left_paper: Vec<i64> = input.iter().map(|(left, _)| *left).collect();
+                let mut right_paper: Vec<i64> = input.iter().map(|(_, right)| *right).collect();
+                let mut delta = 0;
+                while let Some((left_index, left_value)) =
+                    left_paper
+                        .iter()
+                        .enumerate()
+                        .reduce(
+                            |(acc_i, acc_e), (i, e)| {
+                                if acc_e < e {
+                                    (acc_i, acc_e)
+                                } else {
+                                    (i, e)
+                                }
+                            },
+                        )
+                {
+                    if let Some((right_index, right_value)) =
+                        right_paper
+                            .iter()
+                            .enumerate()
+                            .reduce(
+                                |(acc_i, acc_e), (i, e)| {
+                                    if acc_e < e {
+                                        (acc_i, acc_e)
+                                    } else {
+                                        (i, e)
+                                    }
+                                },
+                            )
+                    {
+                        let distance = left_value - right_value;
+                        delta += distance.abs();
+                        left_paper.swap_remove(left_index);
+                        right_paper.swap_remove(right_index);
+                    } else {
+                        break;
+                    };
+                    // break;
+                }
+                delta.to_string()
+            }
+            Solver::Part2 => todo!(),
+        }
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let input = args.path;
+    println!("{:?}", input);
+    let contents = std::fs::read_to_string(input).unwrap();
+    println!("{}", contents);
+    let result = args.solver.solve(contents);
+    println!("{}", result);
+}
