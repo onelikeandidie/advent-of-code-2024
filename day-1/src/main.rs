f378ccd589d33bd76569c5ae58823156f2b3a22d
Pedro Pires (onelikeandidie) <onelikeandidie@proton.me>
2024-12-04 10:44:02 +0000

    Added: Solver for day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -64,13 +64,36 @@ impl Solver {
                         left_paper.swap_remove(left_index);
                         right_paper.swap_remove(right_index);
                     } else {
-                        break;
+                        // Panic in case the right paper has more values which
+                        // should never happen
+                        panic!("The right paper has more values than the left paper");
                     };
-                    // break;
                 }
                 delta.to_string()
             }
-            Solver::Part2 => todo!(),
+            Solver::Part2 => {
+                let input: Vec<(i64, i64)> = input
+                    .split("\n")
+                    .filter(|line| !line.is_empty())
+                    .map(|line| line.split_once(" ").unwrap())
+                    .map(|(left, right)| (left.trim(), right.trim()))
+                    .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
+                    .collect();
+                let left_paper: Vec<i64> = input.iter().map(|(left, _)| *left).collect();
+                let right_paper: Vec<i64> = input.iter().map(|(_, right)| *right).collect();
+                let similarity = left_paper.iter().fold(0, |similarity, left_value| {
+                    similarity
+                        + (left_value
+                            * right_paper.iter().fold(0, |count, right_value| {
+                                if left_value == right_value {
+                                    count + 1
+                                } else {
+                                    count
+                                }
+                            }))
+                });
+                similarity.to_string()
+            }
         }
     }
 }
